7aa0bdc1425c816dac9d83075bf703526bf00d3b
Marc Rollin <rollin.marc@gmail.com>
2014-06-17 10:30:39 +0900

    Updated the footer with the right licensing

--- site/wp-content/plugins/wordfence/tmp/configCache.php
+++ site/wp-content/plugins/wordfence/tmp/configCache.php
@@ -1,4 +1,4 @@
 <?php
 /* Wordfence temporary file security header */
 echo "Nothing to see here!\n"; exit(0);
-?>a:11:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:192:"10cfb3fb8d825061acdc7b6164828b3f52fb78873ef879fe09140301005ae727ffed3c78d6d0ffee8af6980721af835d3de25f20330a7fbb59e5ae470fb84f7f90ed972c1dc12255d4aedf4d17ff4d8f96a57b4ccd6e1097cf9425b59ddce979";s:9:"howGetIPs";s:0:"";s:9:"IPGetFail";s:0:"";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:10:"tourClosed";s:1:"1";s:11:"alertEmails";s:21:"robouden@docomo.ne.jp";s:17:"actUpdateInterval";s:1:"2";s:7:"debugOn";s:1:"0";}
+?>a:12:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:192:"10cfb3fb8d825061acdc7b6164828b3f52fb78873ef879fe09140301005ae727ffed3c78d6d0ffee8af6980721af835d3de25f20330a7fbb59e5ae470fb84f7f90ed972c1dc12255d4aedf4d17ff4d8f96a57b4ccd6e1097cf9425b59ddce979";s:9:"howGetIPs";s:0:"";s:9:"IPGetFail";s:0:"";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:10:"tourClosed";s:1:"1";s:11:"alertEmails";s:21:"robouden@docomo.ne.jp";s:17:"actUpdateInterval";s:1:"2";s:7:"debugOn";s:1:"0";s:25:"liveTraf_ignorePublishers";s:1:"1";}
